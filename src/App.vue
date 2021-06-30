c907cc309bb348456d48e87175210a0193c3864f
Danzstorm <daniel.santos.p@uni.pe>
2021-06-29 22:41:43 -0500

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color= rgb(0,0,36,255)
+      dark
+
+    >
+      <div class="d-flex align-center">
+        <v-img    
+          class="shrink mr-2"
+          contain
+          src="./assets/logo-ccat.png"
+          transition="scale-transition"
+          width="60"
+        />
+
+        <v-app-bar-title class="titulo">CENTRO CULTURAL AVANZADA TECNOLOGICA</v-app-bar-title>
+     
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn dark color=rgb(0,0,36,255)> Inicio</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on , attrs}">
+            <v-btn
+              color=rgb(0,0,36,255)
+              v-bind="attrs"
+              v-on="on"
+            >¿Quienes somos?</v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items1"
+              :key="index"
+            >
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+          <v-menu offset-y>
+          <template v-slot:activator="{ on , attrs}">
+            <v-btn
+              color=rgb(0,0,36,255)
+              v-bind="attrs"
+              v-on="on"
+            >Áreas</v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items2"
+              :key="index"
+            >
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+         <v-menu offset-y>
+          <template v-slot:activator="{ on , attrs}">
+            <v-btn
+              color=rgb(0,0,36,255)
+              v-bind="attrs"
+              v-on="on"
+            >Proyectos</v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items3"
+              :key="index"
+            >
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn dark color=rgb(0,0,36,255)>Productos</v-btn>
+        <v-btn dark color=rgb(0,0,36,255)>Proximos eventos</v-btn>
+
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+      <Sliders/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Sliders from './components/Sliders';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Sliders
+  },
+
+  data: () => ({
+    items1:[
+      {title:'Nosotros'},
+      {title:'Miembros'}
+    ],
+    items2:[
+      {title:'Direcciones'},
+      {title:'Gerencias'}
+    ],
+    items3:[
+      {title:'Proyectos'},
+      {title:'Competitivdad'},
+      {title:'CODEX'}
+    ]
+  }),
+};
+</script>
+
+<style scoped>
+
+.titulo{
+ font-size:0.9rem;
+}
+
+
+
+</style>
---
+++ src/components/Sliders.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class=" font-weight-bold mb-3 mt-4">
+          CONVENIOS
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h2 class="font-weight-bold mb-3 mt-4">
+          AUSPICIADORES
+        </h2>
+      </v-col>
+      <v-col class="mb-4">
+        <h2 class="font-weight-bold mb-3 mt-4">
+          ALIADOS
+        </h2>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-sheet  class="mx-auto" elevation="8"  max-width="800" >
+          <v-slide-group
+            show-arrows="always"
+
+          >
+            <v-slide-item v-for="item in logos" :key="item.id">
+
+              <v-sheet class="ma-4"
+                   height="200"
+                   width="200">
+
+                     <v-img
+                  max-height="200"
+                  max-width="200"
+                  :src="item.direccion"
+                >
+                </v-img>                 
+              </v-sheet>
+            </v-slide-item>
+          </v-slide-group>
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-sheet  class="mx-auto" elevation="8"  max-width="800" >
+          <v-slide-group
+            show-arrows="always"
+
+          >
+            <v-slide-item v-for="item in logos" :key="item.id">
+
+              <v-sheet class="ma-4"
+                   height="200"
+                   width="200">
+
+                     <v-img
+                  max-height="200"
+                  max-width="200"
+                  :src="item.direccion"
+                >
+                </v-img>                 
+              </v-sheet>
+            </v-slide-item>
+          </v-slide-group>
+        </v-sheet>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Sliders",
+  data: () => ({
+    ismounted:false,
+    model:null,
+  }),
+  computed:{
+    logos(){
+      return [
+      { id:1,
+        direccion: require("@/assets/ainad-logo.png"),
+      },
+      { id:2,
+        direccion: require("@/assets/dmc-logo.png"),
+      },
+      { id:3,
+        direccion: require("@/assets/fiis-logo.jpg"),
+      },
+      { id:4,
+        direccion: require("@/assets/nucleo-logo.png"),
+      },
+      { id:5,
+        direccion: require("@/assets/proyecta-logo.jpg"),
+      },
+    ]
+    }
+  },
+  mounted(){
+    this.ismounted=true;
+  },
+};
+</script>
+
+<style></style>
